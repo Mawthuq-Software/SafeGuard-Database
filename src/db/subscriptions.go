123044780ff35a4d2ee8d88e0c74184ba700639b
raspberry.tech <noobie.mine@gmail.com>
2022-09-03 14:15:18 +0000

    added database subscription function (untested)

--- src/db/subscriptions.go
+++ src/db/subscriptions.go
@@ -19,3 +19,31 @@ func GetSubscription(subscriptionID int) (subscription Subscriptions, err error)
 	}
 	return
 }
+
+func GetSubscriptionByName(subscriptionName string) (subscription Subscriptions, err error) {
+	db := DBSystem
+
+	findSub := db.Where("name = ?", subscriptionName).First(&subscription)
+	if errors.Is(findSub.Error, gorm.ErrRecordNotFound) {
+		err = ErrSubscriptionNotFound
+		return
+	} else if findSub.Error != nil {
+		err = ErrQuery
+	}
+	return
+}
+
+func AddSubscription(name string, numKeys int, totalBandwidth int) (err error) {
+	db := DBSystem
+	_, err = GetSubscriptionByName(name)
+	if err != ErrSubscriptionNotFound {
+		newSub := Subscriptions{Name: name, NumberOfKeys: numKeys, TotalBandwidth: totalBandwidth}
+		createErr := db.Create(&newSub)
+		if createErr != nil {
+			err = createErr.Error
+			return
+		}
+		return nil
+	}
+	return
+}
